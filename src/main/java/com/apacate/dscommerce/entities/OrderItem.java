b10500a99c8b55552e77592099a044b379631b52
Antonio pacate <apacate@gmail.com>
2024-12-02 21:34:28 +0200

    database seed

---
+++ src/main/java/com/apacate/dscommerce/entities/OrderItem.java
@@ -0,0 +1,61 @@
+package com.apacate.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_orderItem")
+public class OrderItem {
+
+
+    @EmbeddedId
+    private OrderItemPK id= new OrderItemPK();
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(){
+
+    }
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+
+
+    public Order getOrder() {
+        return id.getOrder();
+
+    }
+
+    public void setOrder(Order order) {
+        this.id.setOrder(order);
+    }
+
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        this.id.setProduct(product);
+    }
+
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
