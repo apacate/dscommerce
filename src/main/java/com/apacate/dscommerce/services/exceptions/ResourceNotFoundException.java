fc796244231b175c784ac44e84c0334c80e10132
Antonio pacate <apacate@gmail.com>
2024-12-18 20:03:26 +0200

    Exception handling

---
+++ src/main/java/com/apacate/dscommerce/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.apacate.dscommerce.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String msg){ 
+        super(msg);
+
+    }
+}
